7a5180d256903d582b96e0cb02c3f58095fe7157
Rishabh Gautam <rishabhgtm1995@yahoo.com>
2023-01-25 15:22:04 +0530

    added required files

---
+++ Swap.java
@@ -0,0 +1,15 @@
+
+
+public class Swap{
+	
+	public static void main(String[] args){
+		int a = 2;
+		int b = 3;
+		System.out.println("Before swapping a = " + a + " and b= " + b );
+		int temp =a;
+		a = b;
+		b = temp;
+		System.out.println("After swapping a = " + a + " and b= " + b );
+		
+	}
+}
